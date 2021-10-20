4768d80d5ce7e8e8d657d6faddd7adea509b265f
gt6x2014 <gt6x2014@gmail.com>
2021-10-20 23:09:37 +0900

    1st commit

---
+++ main.c
@@ -0,0 +1,63 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+#define SIZE 2
+
+void print_matrix(int matrix[SIZE][SIZE]) {
+	int i, j;
+	for (i = 0; i < SIZE; i++) {
+		for (j = 0; j < SIZE; j++) {
+			printf("%3d ", matrix[i][j]);
+		}
+		printf("\n");
+	}
+}
+
+void add_matrix(int result[SIZE][SIZE], int a[SIZE][SIZE], int b[SIZE][SIZE]) {
+	int i, j;
+	for (i = 0; i < SIZE; i++) {
+		for (j = 0; j < SIZE; j++) {
+			result[i][j] = a[i][j] + b[i][j];
+		}
+	}
+}
+
+void sub_matrix(int result[SIZE][SIZE], int a[SIZE][SIZE], int b[SIZE][SIZE]) {
+	int i, j;
+	for (i = 0; i < SIZE; i++) {
+		for (j = 0; j < SIZE; j++) {
+			result[i][j] = a[i][j] - b[i][j];
+		}
+	}
+}
+
+int main() {
+	int a[SIZE][SIZE];
+	int b[SIZE][SIZE];
+	int result[SIZE][SIZE];
+	int i, j;
+
+	srand((unsigned)time(NULL));
+
+	for (i = 0; i < SIZE; i++) {
+		for (j = 0; j < SIZE; j++) {
+			a[i][j] = rand() % 10;
+			b[i][j] = rand() % 10;
+		}
+	}
+
+	printf("행렬 A\n");
+	print_matrix(a);
+	printf("\n행렬 B\n");
+	print_matrix(b);
+
+	printf("\n행렬 A+B\n");
+	add_matrix(result, a, b);
+	print_matrix(result);
+
+	printf("\n행렬 A-B\n");
+	sub_matrix(result, a, b);
+	print_matrix(result);
+
+	return 0;
+}
