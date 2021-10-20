b8327b3ed7a4c04ffe92346664be575e5748d886
gt6x2014 <gt6x2014@gmail.com>
2021-10-20 23:13:17 +0900

    mul function added

--- main.c
+++ main.c
@@ -31,6 +31,19 @@ void sub_matrix(int result[SIZE][SIZE], int a[SIZE][SIZE], int b[SIZE][SIZE]) {
 	}
 }
 
+void mul_matrix(int result[SIZE][SIZE], int a[SIZE][SIZE], int b[SIZE][SIZE]) {
+	int i, j, k;
+	for (i = 0; i < SIZE; i++) {
+		for (j = 0; j < SIZE; j++) {
+			result[i][j] = 0;
+
+			for (k = 0; k < SIZE; k++) {
+				result[i][j] += a[i][k] * b[k][j];
+			}
+		}
+	}
+}
+
 int main() {
 	int a[SIZE][SIZE];
 	int b[SIZE][SIZE];
@@ -51,12 +64,16 @@ int main() {
 	printf("\n행렬 B\n");
 	print_matrix(b);
 
-	printf("\n행렬 A+B\n");
+	printf("\n행렬 A + B\n");
 	add_matrix(result, a, b);
 	print_matrix(result);
 
-	printf("\n행렬 A-B\n");
+	printf("\n행렬 A - B\n");
 	sub_matrix(result, a, b);
+	print_matrix(result);
+
+	printf("\n행렬 A * B\n");
+	mul_matrix(result, a, b);
 	print_matrix(result);
 
 	return 0;
